fee3a86e7f0612ad004f10adde4e8da7d64a9696
Алтнбайр <altnbair.moshuldaeva@yandex.ru>
2020-01-27 22:00:31 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. 
-        </p> hhhh
+        </p> hello
         <a
           className="App-link"
           href="https://reactjs.org"
